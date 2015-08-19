84757abdee41d87679fd34492c25e215357f9d9a
hehe19900512 <870347271@qq.com>
2015-08-19 18:02:25 +0800

    Initial Commit

---
+++ ActivityView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ActivityView
+//
+//  Created by yanshu on 15/8/19.
+//  Copyright (c) 2015年 yanshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ActivityView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActivityView
+//
+//  Created by yanshu on 15/8/19.
+//  Copyright (c) 2015年 yanshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
